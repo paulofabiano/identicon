dc1a966fce4f78201c1001b70577e621c65c729a
Paulo Langer <paulo.langer@xing.com>
2021-05-05 15:14:25 +0100

    adding color to image struct and refactoring pick_color method to return %Image struct with color

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -4,13 +4,18 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input()
+    |> pick_color()
   end
 
-  def hash_input(input) do
+  defp hash_input(input) do
     hex =
       :crypto.hash(:md5, input)
       |> :binary.bin_to_list()
 
     %Image{hex: hex}
+  end
+
+  defp pick_color(%Image{hex: [r, g, b | _tail]} = image) do
+    %Image{image | color: {r, g, b}}
   end
 end
